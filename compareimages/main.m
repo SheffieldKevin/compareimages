0974efcddafdb0bb5643536857aac553a964fa47
Kevin Meaney <ktam@yvs.eu.com>
2014-02-25 10:20:08 +0000

    Initial Commit

---
+++ compareimages/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  compareimages
+//
+//  Created by Kevin Meaney on 25/02/2014.
+//  Copyright (c) 2014 Kevin Meaney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
